fbcd7d2e14969c30901d63d507a8fc2d318bf51e
Jared Qin <jaredqin@gmail.com>
2023-03-19 21:59:35 +1100

    add an e2e test case for POST /scan API

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -19,6 +19,73 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('KNS Pricing and Discounting Service');
+  });
+
+  it('/scan (POST)', async () => {
+    const excepted = {
+      success: true,
+      message: 'Prices and discounts have been applied.',
+      data: {
+        products: [
+          {
+            name: 'apple',
+            price: 50,
+            resultedPrice: 50,
+          },
+          {
+            name: 'banana',
+            price: 40,
+            resultedPrice: 0,
+            discountCode: '2for3',
+          },
+          {
+            name: 'banana',
+            price: 40,
+            resultedPrice: 40,
+          },
+          {
+            name: 'potato',
+            price: 26,
+            resultedPrice: 26,
+          },
+          {
+            name: 'tomato',
+            price: 30,
+            resultedPrice: 30,
+          },
+          {
+            name: 'banana',
+            price: 40,
+            resultedPrice: 40,
+          },
+          {
+            name: 'potato',
+            price: 26,
+            resultedPrice: 13,
+            discountCode: 'buy1get1halfprice',
+          },
+        ],
+        totalPrice: 199,
+      },
+    };
+
+    const body = {
+      products: [
+        'apple',
+        'banana',
+        'banana',
+        'potato',
+        'tomato',
+        'banana',
+        'potato',
+      ],
+    };
+    const response = await request(app.getHttpServer())
+      .post('/scan')
+      .send(body)
+      .expect(HttpStatus.CREATED);
+
+    expect(response.body).toEqual(excepted);
   });
 });
